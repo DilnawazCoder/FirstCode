46db46238ebc005e541479e95bd88bae8c03aac4
DilnawazCoder <117380845+DilnawazCoder@users.noreply.github.com>
2022-11-03 09:22:28 -0700

    Add files via upload

---
+++ student2/student2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student2
+{
+    internal class Student
+    {
+
+
+        //private  int studentid;
+        //private string studentName;
+        //private int marks;
+        //private char grade;
+        int studentid;
+        string studentName;
+        int marks;
+        //char grade;
+
+        public Student()
+        {
+        }
+        public Student(int studentid, string studentName, int marks)
+        {
+            this.studentid = studentid;
+            this.studentName = studentName;
+            this.marks = marks;
+         
+        }
+        public void DisplayDetails()
+
+        {
+
+            
+        // Console.WriteLine("name: " + studentName + " " + "studentid: " + studentid + " " + "marks: " + marks + " " + "grade is: " + calculateGrade());
+         // return "name" + studentName + " " + "studentid" + studentid + " " + "marks" + marks + " " + "grade is" + calculateGrade();
+            
+            Console.WriteLine(" studentid=" + studentid);
+            Console.WriteLine("  studentName=" + studentName);
+            Console.WriteLine("  marks=" + marks);
+            Console.WriteLine("grade is=" + calculateGrade());
+             
+
+        }
+        public char calculateGrade()
+
+        {
+           
+            if (marks > 90)
+            {
+
+               return 'A'; 
+               
+               
+            }
+            else if (marks > 81 && marks < 90)
+            {
+                return 'B';
+            }
+            else if (marks > 71 && marks < 80)
+            {
+                return 'C';
+            }
+            else if (marks > 61 && marks < 70)
+            {
+                return'D';
+            }
+            else
+            {
+                return 'C';
+            }
+            
+           
+
+        }
+
+            public static void Main()
+            {
+                Student st = new Student(9029, "dil", 39);
+                st.DisplayDetails();
+                Console.ReadLine(); 
+
+            
+            }
+
+        }
+    
+    
+    }
+
+
+
+
